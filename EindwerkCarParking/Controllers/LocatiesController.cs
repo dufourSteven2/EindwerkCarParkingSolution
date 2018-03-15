455bae4d3cedd7a4354ae902898fb4cf6c408037
steven <nonkelchameau@gmail.com>
2018-03-15 18:26:03 +0100

    Referentie naar Lib en aanmaak controllers

---
+++ EindwerkCarParking/Controllers/LocatiesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EindwerkCarParking.Models;
+using EindwerkCarParkingLib;
+
+namespace EindwerkCarParking.Controllers
+{
+    public class LocatiesController : ApiController
+    {
+        private EindwerkCarParkingContext db = new EindwerkCarParkingContext();
+
+        // GET: api/Locaties
+        public IQueryable<Locatie> GetLocaties()
+        {
+            return db.Locaties;
+        }
+
+        // GET: api/Locaties/5
+        [ResponseType(typeof(Locatie))]
+        public async Task<IHttpActionResult> GetLocatie(int id)
+        {
+            Locatie locatie = await db.Locaties.FindAsync(id);
+            if (locatie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(locatie);
+        }
+
+        // PUT: api/Locaties/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutLocatie(int id, Locatie locatie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != locatie.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(locatie).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocatieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Locaties
+        [ResponseType(typeof(Locatie))]
+        public async Task<IHttpActionResult> PostLocatie(Locatie locatie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Locaties.Add(locatie);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = locatie.Id }, locatie);
+        }
+
+        // DELETE: api/Locaties/5
+        [ResponseType(typeof(Locatie))]
+        public async Task<IHttpActionResult> DeleteLocatie(int id)
+        {
+            Locatie locatie = await db.Locaties.FindAsync(id);
+            if (locatie == null)
+            {
+                return NotFound();
+            }
+
+            db.Locaties.Remove(locatie);
+            await db.SaveChangesAsync();
+
+            return Ok(locatie);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LocatieExists(int id)
+        {
+            return db.Locaties.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ EindwerkCarParking/Controllers/ParkingsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EindwerkCarParking.Models;
+using EindwerkCarParkingLib;
+
+namespace EindwerkCarParking.Controllers
+{
+    public class ParkingsController : ApiController
+    {
+        private EindwerkCarParkingContext db = new EindwerkCarParkingContext();
+
+        // GET: api/Parkings
+        public IQueryable<Parking> GetParkings()
+        {
+            return db.Parkings;
+        }
+
+        // GET: api/Parkings/5
+        [ResponseType(typeof(Parking))]
+        public async Task<IHttpActionResult> GetParking(int id)
+        {
+            Parking parking = await db.Parkings.FindAsync(id);
+            if (parking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(parking);
+        }
+
+        // PUT: api/Parkings/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutParking(int id, Parking parking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != parking.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(parking).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ParkingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Parkings
+        [ResponseType(typeof(Parking))]
+        public async Task<IHttpActionResult> PostParking(Parking parking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Parkings.Add(parking);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = parking.Id }, parking);
+        }
+
+        // DELETE: api/Parkings/5
+        [ResponseType(typeof(Parking))]
+        public async Task<IHttpActionResult> DeleteParking(int id)
+        {
+            Parking parking = await db.Parkings.FindAsync(id);
+            if (parking == null)
+            {
+                return NotFound();
+            }
+
+            db.Parkings.Remove(parking);
+            await db.SaveChangesAsync();
+
+            return Ok(parking);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ParkingExists(int id)
+        {
+            return db.Parkings.Count(e => e.Id == id) > 0;
+        }
+    }
+}
