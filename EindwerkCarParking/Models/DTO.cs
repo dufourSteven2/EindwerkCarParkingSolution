fb7d2557f61b263ac4902dc3ffd4de2e7c0f41b5
steven <nonkelchameau@gmail.com>
2018-03-15 19:23:54 +0100

    De dto's toegevoegd

---
+++ EindwerkCarParking/Models/DTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EindwerkCarParking.Models
+{
+    public class ParkingsDTO
+    {
+        public int Id { get; set; }
+        public string ParkingNaam { get; set; }
+        public string GemeenteGemeenteNaam { get; set; }
+
+    }
+
+    public class ParkingsDetailDTO
+    {
+        public int Id { get; set; }
+        public string ParkingNaam { get; set; }
+        public string LocatieStraat { get; set; }
+        public string GemeenteGemeenteNaam { get; set; }
+        public string LandLandNaam { get; set; }
+    }
+
+    public class EigenaarDTO
+    {
+        public int Id { get; set; }
+        public string EigenaarNaam { get; set; }
+    }
+
+    public class GemeenteDTO
+    {
+        public int Id { get; set; }
+        public string GemeenteNaam { get; set; }
+    }
+
+    public class LocatieDTO
+    {
+        public int Id { get; set; }
+        public string Straat { get; set; }
+    }
+
+    public class LandDTO
+    {
+        public int Id { get; set; }
+        public string LandNaam { get; set; }
+    }
+    public class SoortDTO
+    {
+        public int Id { get; set; }
+        public string SoortNaam { get; set; }
+    }
+}
