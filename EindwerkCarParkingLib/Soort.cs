94c564db570709a174724a10f5b68ee57f901f51
steven <nonkelchameau@gmail.com>
2018-03-15 17:53:44 +0100

    Begin Eindwerk 2 projecten Web_Api en ClassLibrary

---
+++ EindwerkCarParkingLib/Soort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EindwerkCarParkingLib
+{
+    public class Soort
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string SoortNaam { get; set; }
+
+        public virtual ICollection<Parking> Parkings { get; set; }
+    }
+}
