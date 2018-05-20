126c8aa1e73e57c1eb190a9fd98e4678ebf1425d
steven <nonkelchameau@gmail.com>
2018-05-20 08:17:58 +0200

    Enkel homecontroller de rest uncomment + wat opkuiswerk

--- EindwerkCarParkingCore/Controllers/ParkingsController.cs
+++ EindwerkCarParkingCore/Controllers/ParkingsController.cs
@@ -1,158 +1,158 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using EindwerkCarParkingCore.Data;
-using EindwerkCarParkingLib;
-using EindwerkCarParkingCore.Models;
-using AutoMapper.QueryableExtensions;
-using AutoMapper;
-using System.Web.Http.Description;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using EindwerkCarParkingCore.Data;
+//using EindwerkCarParkingLib;
+//using EindwerkCarParkingCore.Models;
+//using AutoMapper.QueryableExtensions;
+//using AutoMapper;
+//using System.Web.Http.Description;
 
-namespace EindwerkCarParkingCore.Controllers
-{
-    [Produces("application/json")]
-    [Route("api/Parkings")]
-    public class ParkingsController : Controller
-    {
-        private readonly EindwerkCarParkingContext _context;
-        private readonly IMapper _mapper;
+//namespace EindwerkCarParkingCore.Controllers
+//{
+//    [Produces("application/json")]
+//    [Route("api/Parkings")]
+//    public class ParkingsController : Controller
+//    {
+//        private readonly EindwerkCarParkingContext _context;
+//        private readonly IMapper _mapper;
 
-        public ParkingsController(EindwerkCarParkingContext context)
-        {
-            _context = context;
-            //IMapper _Mapper;
-        }
+//        public ParkingsController(EindwerkCarParkingContext context)
+//        {
+//            _context = context;
+//            //IMapper _Mapper;
+//        }
 
-        // GET: api/Parkings
-        [HttpGet]
-        public IEnumerable<ParkingsDTO> GetParkings()
-        {
-            var parkings = _context.Parkings.ProjectTo<ParkingsDTO>();
-            return parkings;
-            //return _context.Parkings;
-        }
+//        // GET: api/Parkings
+//        [HttpGet]
+//        public IEnumerable<ParkingsDTO> GetParkings()
+//        {
+//            var parkings = _context.Parkings.ProjectTo<ParkingsDTO>();
+//            return parkings;
+//            //return _context.Parkings;
+//        }
 
-        // GET: api/Parkings/5
-        [HttpGet("{id}")]
-        //[ResponseType(typeof(ParkingsDetailDTO))]
-        public async Task<IActionResult> GetParking([FromRoute] int id)
-        {
-            //var parking = await _context.Parkings.ProjectTo<ParkingsDetailDTO>().SingleOrDefaultAsync(p => p.Id == id);
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var parking = await _context.Parkings.ProjectTo<ParkingsDetailDTO>().SingleOrDefaultAsync(m => m.Id == id);
-            //var parking = await _context.Parkings.SingleOrDefaultAsync(m => m.Id == id);
+//        // GET: api/Parkings/5
+//        [HttpGet("{id}")]
+//        //[ResponseType(typeof(ParkingsDetailDTO))]
+//        public async Task<IActionResult> GetParking([FromRoute] int id)
+//        {
+//            //var parking = await _context.Parkings.ProjectTo<ParkingsDetailDTO>().SingleOrDefaultAsync(p => p.Id == id);
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
+//            var parking = await _context.Parkings.ProjectTo<ParkingsDetailDTO>().SingleOrDefaultAsync(m => m.Id == id);
+//            //var parking = await _context.Parkings.SingleOrDefaultAsync(m => m.Id == id);
 
-            if (parking == null)
-            {
-                return NotFound();
-            }
+//            if (parking == null)
+//            {
+//                return NotFound();
+//            }
 
-            return Ok(parking);
-        }
+//            return Ok(parking);
+//        }
 
-        // PUT: api/Parkings/5
-        [HttpPut("{id}")]
-        //public async Task<IHttpActionResult> PutParking(int id, ParkingsDetailDTO parkingsDetailDTO)
-        public async Task<IActionResult> PutParking([FromRoute] int id, [FromBody] ParkingsDetailDTO parkingsDetailDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            Parking parking = Mapper.Map<Parking>(parkingsDetailDTO);
-            _context.Set<Parking>().Attach(parking); //hier de update
-            _context.Entry(parking).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+//        // PUT: api/Parkings/5
+//        [HttpPut("{id}")]
+//        //public async Task<IHttpActionResult> PutParking(int id, ParkingsDetailDTO parkingsDetailDTO)
+//        public async Task<IActionResult> PutParking([FromRoute] int id, [FromBody] ParkingsDetailDTO parkingsDetailDTO)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
+//            Parking parking = Mapper.Map<Parking>(parkingsDetailDTO);
+//            _context.Set<Parking>().Attach(parking); //hier de update
+//            _context.Entry(parking).State = EntityState.Modified;
+//            await _context.SaveChangesAsync();
 
-            return Ok(parkingsDetailDTO);
+//            return Ok(parkingsDetailDTO);
 
 
-            //if (id != parkingsDetailDTO.Id)
-            //{
-            //    return BadRequest();
-            //}
+//            //if (id != parkingsDetailDTO.Id)
+//            //{
+//            //    return BadRequest();
+//            //}
 
-            //_context.Entry(parkingsDetailDTO).State = EntityState.Modified;
+//            //_context.Entry(parkingsDetailDTO).State = EntityState.Modified;
 
-            //try
-            //{
-            //    await _context.SaveChangesAsync();
-            //}
-            //catch (DbUpdateConcurrencyException)
-            //{
-            //    if (!ParkingExists(id))
-            //    {
-            //        return NotFound();
-            //    }
-            //    else
-            //    {
-            //        throw;
-            //    }
-            //}
+//            //try
+//            //{
+//            //    await _context.SaveChangesAsync();
+//            //}
+//            //catch (DbUpdateConcurrencyException)
+//            //{
+//            //    if (!ParkingExists(id))
+//            //    {
+//            //        return NotFound();
+//            //    }
+//            //    else
+//            //    {
+//            //        throw;
+//            //    }
+//            //}
 
-            //return NoContent();
-        }
+//            //return NoContent();
+//        }
 
-        // POST: api/Parkings
-        [HttpPost]
-        public async Task<IActionResult> PostParking([FromBody] Parking parking)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//        // POST: api/Parkings
+//        [HttpPost]
+//        public async Task<IActionResult> PostParking([FromBody] Parking parking)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
 
-            _context.Parkings.Add(parking);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (ParkingExists(parking.Id))
-                {
-                    return new StatusCodeResult(StatusCodes.Status409Conflict);
-                }
-                else
-                {
-                    throw;
-                }
-            }
+//            _context.Parkings.Add(parking);
+//            try
+//            {
+//                await _context.SaveChangesAsync();
+//            }
+//            catch (DbUpdateException)
+//            {
+//                if (ParkingExists(parking.Id))
+//                {
+//                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+//                }
+//                else
+//                {
+//                    throw;
+//                }
+//            }
 
-            return CreatedAtAction("GetParking", new { id = parking.Id }, parking);
-        }
+//            return CreatedAtAction("GetParking", new { id = parking.Id }, parking);
+//        }
 
-        // DELETE: api/Parkings/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteParking([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//        // DELETE: api/Parkings/5
+//        [HttpDelete("{id}")]
+//        public async Task<IActionResult> DeleteParking([FromRoute] int id)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
 
-            var parking = await _context.Parkings.SingleOrDefaultAsync(m => m.Id == id);
-            if (parking == null)
-            {
-                return NotFound();
-            }
+//            var parking = await _context.Parkings.SingleOrDefaultAsync(m => m.Id == id);
+//            if (parking == null)
+//            {
+//                return NotFound();
+//            }
 
-            _context.Parkings.Remove(parking);
-            await _context.SaveChangesAsync();
+//            _context.Parkings.Remove(parking);
+//            await _context.SaveChangesAsync();
 
-            return Ok(parking);
-        }
+//            return Ok(parking);
+//        }
 
-        private bool ParkingExists(int id)
-        {
-            return _context.Parkings.Any(e => e.Id == id);
-        }
-    }
-}
+//        private bool ParkingExists(int id)
+//        {
+//            return _context.Parkings.Any(e => e.Id == id);
+//        }
+//    }
+//}
--- EindwerkCarParkingCore/Controllers/SoortsController.cs
+++ EindwerkCarParkingCore/Controllers/SoortsController.cs
@@ -1,143 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using EindwerkCarParkingCore.Data;
-using EindwerkCarParkingLib;
-using EindwerkCarParkingCore.Models;
-using AutoMapper.QueryableExtensions;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using EindwerkCarParkingCore.Data;
+//using EindwerkCarParkingLib;
+//using EindwerkCarParkingCore.Models;
+//using AutoMapper.QueryableExtensions;
 
-namespace EindwerkCarParkingCore.Controllers
-{
-    [Produces("application/json")]
-    [Route("api/Soorts")]
-    public class SoortsController : Controller
-    {
-        private readonly EindwerkCarParkingContext _context;
+//namespace EindwerkCarParkingCore.Controllers
+//{
+//    [Produces("application/json")]
+//    [Route("api/Soorts")]
+//    public class SoortsController : Controller
+//    {
+//        private readonly EindwerkCarParkingContext _context;
 
-        public SoortsController(EindwerkCarParkingContext context)
-        {
-            _context = context;
-        }
+//        public SoortsController(EindwerkCarParkingContext context)
+//        {
+//            _context = context;
+//        }
 
-        // GET: api/Soorts
-        [HttpGet]
-        public IEnumerable<SoortDTO> GetSoorts()
-        {
-            //return _context.Soorts;
-            return _context.Soorts.ProjectTo<SoortDTO>();
-        }
+//        // GET: api/Soorts
+//        [HttpGet]
+//        public IEnumerable<SoortDTO> GetSoorts()
+//        {
+//            //return _context.Soorts;
+//            return _context.Soorts.ProjectTo<SoortDTO>();
+//        }
 
-        // GET: api/Soorts/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetSoort([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//        // GET: api/Soorts/5
+//        [HttpGet("{id}")]
+//        public async Task<IActionResult> GetSoort([FromRoute] int id)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
 
-            var soort = await _context.Soorts.SingleOrDefaultAsync(m => m.Id == id);
+//            var soort = await _context.Soorts.SingleOrDefaultAsync(m => m.Id == id);
 
-            if (soort == null)
-            {
-                return NotFound();
-            }
+//            if (soort == null)
+//            {
+//                return NotFound();
+//            }
 
-            return Ok(soort);
-        }
+//            return Ok(soort);
+//        }
 
-        // PUT: api/Soorts/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSoort([FromRoute] int id, [FromBody] Soort soort)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//        // PUT: api/Soorts/5
+//        [HttpPut("{id}")]
+//        public async Task<IActionResult> PutSoort([FromRoute] int id, [FromBody] Soort soort)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
 
-            if (id != soort.Id)
-            {
-                return BadRequest();
-            }
+//            if (id != soort.Id)
+//            {
+//                return BadRequest();
+//            }
 
-            _context.Entry(soort).State = EntityState.Modified;
+//            _context.Entry(soort).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SoortExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+//            try
+//            {
+//                await _context.SaveChangesAsync();
+//            }
+//            catch (DbUpdateConcurrencyException)
+//            {
+//                if (!SoortExists(id))
+//                {
+//                    return NotFound();
+//                }
+//                else
+//                {
+//                    throw;
+//                }
+//            }
 
-            return NoContent();
-        }
+//            return NoContent();
+//        }
 
-        // POST: api/Soorts
-        [HttpPost]
-        public async Task<IActionResult> PostSoort([FromBody] Soort soort)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//        // POST: api/Soorts
+//        [HttpPost]
+//        public async Task<IActionResult> PostSoort([FromBody] Soort soort)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
 
-            _context.Soorts.Add(soort);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (SoortExists(soort.Id))
-                {
-                    return new StatusCodeResult(StatusCodes.Status409Conflict);
-                }
-                else
-                {
-                    throw;
-                }
-            }
+//            _context.Soorts.Add(soort);
+//            try
+//            {
+//                await _context.SaveChangesAsync();
+//            }
+//            catch (DbUpdateException)
+//            {
+//                if (SoortExists(soort.Id))
+//                {
+//                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+//                }
+//                else
+//                {
+//                    throw;
+//                }
+//            }
 
-            return CreatedAtAction("GetSoort", new { id = soort.Id }, soort);
-        }
+//            return CreatedAtAction("GetSoort", new { id = soort.Id }, soort);
+//        }
 
-        // DELETE: api/Soorts/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteSoort([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//        // DELETE: api/Soorts/5
+//        [HttpDelete("{id}")]
+//        public async Task<IActionResult> DeleteSoort([FromRoute] int id)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
 
-            var soort = await _context.Soorts.SingleOrDefaultAsync(m => m.Id == id);
-            if (soort == null)
-            {
-                return NotFound();
-            }
+//            var soort = await _context.Soorts.SingleOrDefaultAsync(m => m.Id == id);
+//            if (soort == null)
+//            {
+//                return NotFound();
+//            }
 
-            _context.Soorts.Remove(soort);
-            await _context.SaveChangesAsync();
+//            _context.Soorts.Remove(soort);
+//            await _context.SaveChangesAsync();
 
-            return Ok(soort);
-        }
+//            return Ok(soort);
+//        }
 
-        private bool SoortExists(int id)
-        {
-            return _context.Soorts.Any(e => e.Id == id);
-        }
-    }
-}
+//        private bool SoortExists(int id)
+//        {
+//            return _context.Soorts.Any(e => e.Id == id);
+//        }
+//    }
+//}
