6e504f465162a9ed5709e4197209ca85db51ed29
VanderschaegheBenny <vanderschaeghe.benny@hotmail.com>
2018-05-22 23:20:59 +0200

    add     [Table("AspNetUsers") in parkinguser klasse, zou foutmelding voor database kunnen oplossen

--- EindwerkCarParkingCore/Data/Entities/ParkingUser.cs
+++ EindwerkCarParkingCore/Data/Entities/ParkingUser.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EindwerkCarParkingCore.Data.Entities
 {
+    [Table("AspNetUsers")]
     public class ParkingUser : IdentityUser
     {
         public string Voornaam { get; set; }
