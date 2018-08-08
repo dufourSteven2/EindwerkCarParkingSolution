e44db92bc254cac71426ac59cdce5fde3666c1c7
steven <nonkelchameau@gmail.com>
2018-08-08 17:42:22 +0200

    nu met weergave totaal

--- EindwerkCarParkingCore/Models/DTO.cs
+++ EindwerkCarParkingCore/Models/DTO.cs
@@ -30,6 +30,7 @@ namespace EindwerkCarParkingCore.Models
         public string GemeenteGemeenteNaam { get; set; }
         public string SoortSoortNaam { get; set; }
         public decimal Percentage { get; set; }
+        public decimal Totaal { get; set; } //test voor vue
         public string LandLandNaam { get; set; }
         public string ParkingUsersEigenaarNaam { get; set; }
         public bool PublicatieToelating { get; set; }  //wanneer deze bool op true staat, kan de parking getoond worden op de website
